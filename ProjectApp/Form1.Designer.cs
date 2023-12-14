aba89829b56d9e6f9ada7ee3b29a85006654b9a1
Fabriceelono <r0912443@student.thomamsore.be>
2023-12-15 00:37:02 +0100

    Initial commit

---
+++ ProjectApp/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace ProjectApp
+{
+    partial class ProjectApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Projects = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.Label();
+            this.newButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.descriptonTextBox = new System.Windows.Forms.TextBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.projectAppView = new System.Windows.Forms.DataGridView();
+            this.startDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.endDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.projectAppView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Projects
+            // 
+            this.Projects.Font = new System.Drawing.Font("Microsoft YaHei UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Projects.ForeColor = System.Drawing.Color.White;
+            this.Projects.Location = new System.Drawing.Point(27, 23);
+            this.Projects.Name = "Projects";
+            this.Projects.Size = new System.Drawing.Size(219, 70);
+            this.Projects.TabIndex = 0;
+            this.Projects.Text = "Projects";
+            // 
+            // title
+            // 
+            this.title.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(323, 29);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(219, 70);
+            this.title.TabIndex = 1;
+            this.title.Text = " Title";
+            // 
+            // description
+            // 
+            this.description.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.description.ForeColor = System.Drawing.Color.White;
+            this.description.Location = new System.Drawing.Point(323, 99);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(292, 70);
+            this.description.TabIndex = 2;
+            this.description.Text = "Description";
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(71, 99);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(108, 35);
+            this.newButton.TabIndex = 3;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(334, 186);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(108, 35);
+            this.editButton.TabIndex = 4;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(614, 50);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(682, 22);
+            this.titleTextBox.TabIndex = 5;
+            // 
+            // descriptonTextBox
+            // 
+            this.descriptonTextBox.Location = new System.Drawing.Point(614, 129);
+            this.descriptonTextBox.Name = "descriptonTextBox";
+            this.descriptonTextBox.Size = new System.Drawing.Size(682, 22);
+            this.descriptonTextBox.TabIndex = 6;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1055, 186);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(108, 35);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(1188, 186);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(108, 35);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // projectAppView
+            // 
+            this.projectAppView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.projectAppView.BackgroundColor = System.Drawing.Color.White;
+            this.projectAppView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.projectAppView.Location = new System.Drawing.Point(321, 279);
+            this.projectAppView.Name = "projectAppView";
+            this.projectAppView.RowHeadersWidth = 51;
+            this.projectAppView.RowTemplate.Height = 24;
+            this.projectAppView.Size = new System.Drawing.Size(975, 491);
+            this.projectAppView.TabIndex = 9;
+            // 
+            // startDatePicker
+            // 
+            this.startDatePicker.CustomFormat = "";
+            this.startDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.startDatePicker.Location = new System.Drawing.Point(80, 292);
+            this.startDatePicker.Name = "startDatePicker";
+            this.startDatePicker.Size = new System.Drawing.Size(226, 22);
+            this.startDatePicker.TabIndex = 10;
+            // 
+            // endDatePicker
+            // 
+            this.endDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.endDatePicker.Location = new System.Drawing.Point(80, 345);
+            this.endDatePicker.Name = "endDatePicker";
+            this.endDatePicker.Size = new System.Drawing.Size(226, 22);
+            this.endDatePicker.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 29);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Start";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(17, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 29);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "End";
+            // 
+            // ProjectApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1655, 864);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.endDatePicker);
+            this.Controls.Add(this.startDatePicker);
+            this.Controls.Add(this.projectAppView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.descriptonTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.Projects);
+            this.Name = "ProjectApp";
+            this.Text = "Project App";
+            this.Load += new System.EventHandler(this.ProjectApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.projectAppView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Projects;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox descriptonTextBox;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView projectAppView;
+        private System.Windows.Forms.DateTimePicker startDatePicker;
+        private System.Windows.Forms.DateTimePicker endDatePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProjectApp/Form1.cs
@@ -0,0 +1,317 @@
+﻿using ProjectApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace ProjectApp
+{
+    public partial class ProjectApp : Form
+    {
+        private DataTable projectApp = new DataTable();
+        private SQLiteConnection sqliteConnection;
+        public ProjectApp()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+        }
+
+        private void InitializeDatabase()
+        {
+            // Set your SQLite database file path
+            string dbFilePath = "C:\\Users\\Fabrice ELONO PISETH\\source\\repos\\ProjectApp\\ProjectApp\\projects.db";
+
+            // Connection string for SQLite
+            string connectionString = $"Data Source={dbFilePath};Version=3;";
+
+            // Initialize SQLite connection
+            sqliteConnection = new SQLiteConnection(connectionString);
+
+            // Create the projects table if it doesn't exist
+            using (SQLiteCommand command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS projects (Title TEXT, Description TEXT, StartDate DATE, EndDate DATE)", sqliteConnection))
+            {
+                sqliteConnection.Open();
+                command.ExecuteNonQuery();
+                sqliteConnection.Close();
+            }
+        }
+
+        //Creating our data tabe I will use SQLite later (it will store our data)
+        /*DataTable projectApp = new DataTable();*/
+        bool isEditing = false;
+
+        private void ProjectApp_Load(object sender, EventArgs e)
+        {
+            //Now creating some columns for title and description
+            projectApp.Columns.Add("Id");
+            projectApp.Columns.Add("Title");
+            projectApp.Columns.Add("Description");
+            projectApp.Columns.Add("StartDate");
+            projectApp.Columns.Add("EndDate");
+
+            //Now pointing the datagridview to display our data source
+            projectAppView.DataSource = projectApp;
+            // Load data from the SQLite database when the form loads
+            LoadDataFromDatabase();
+        }
+        private void LoadDataFromDatabase()
+        {
+            // Clear existing data in the DataTable
+            projectApp.Rows.Clear();
+
+            // Retrieve data from the SQLite database and populate the DataTable
+            using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM projects", sqliteConnection))
+            {
+                sqliteConnection.Open();
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader["Id"]);
+                        string title = reader["Title"].ToString();
+                        string description = reader["Description"].ToString();
+                        // Check if "StartDate" is DBNull or null
+                        DateTime startDate = (reader["StartDate"] == DBNull.Value || reader["StartDate"] == null) ? DateTime.MinValue : Convert.ToDateTime(reader["StartDate"]);
+
+                        // Check if "EndDate" is DBNull or null
+                        DateTime endDate = (reader["EndDate"] == DBNull.Value || reader["EndDate"] == null) ? DateTime.MinValue : Convert.ToDateTime(reader["EndDate"]);
+
+                        projectApp.Rows.Add(id, title, description, startDate, endDate);
+                    
+                    // Concatenate "Project" to the Id for display
+                    // string displayId = "Project " + id;
+
+                    // Add the retrieved values to the DataTable
+                    //projectApp.Rows.Add( id,title, description);
+                    }
+                }
+                sqliteConnection.Close();
+            }
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {//new text box
+            titleTextBox.Text = "";  // Deleting or erasing the data in the title text box to be able to add a new one
+            descriptonTextBox.Text = "";
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            //extracting a past item (title and description)
+            isEditing = true; //to be able to track if it is editing
+            //Use text fields with data from the table
+            //retreive data from our database created at projectApp
+            titleTextBox.Text = projectApp.Rows[projectAppView.CurrentCell.RowIndex].ItemArray[1].ToString();
+            descriptonTextBox.Text = projectApp.Rows[projectAppView.CurrentCell.RowIndex].ItemArray[2].ToString();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int rowIndex = projectAppView.CurrentCell.RowIndex;
+
+                // Check if the selected cell is valid
+                if (rowIndex >= 0 && rowIndex < projectApp.Rows.Count)
+                {
+                    // Get values from the selected row
+                    string title = projectApp.Rows[rowIndex]["Title"].ToString();
+                    string description = projectApp.Rows[rowIndex]["Description"].ToString();
+
+                    // Delete the row from the DataTable
+                    projectApp.Rows[rowIndex].Delete();
+
+                    // Delete the corresponding record from the database
+                    DeleteRecordFromDatabase(title, description);
+
+                    // Refresh the DataGridView
+                    projectApp.AcceptChanges();
+                    projectAppView.Refresh();
+
+                    ShowPopup("Successfully Deleted!");
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid row index: {rowIndex}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+        private void DeleteRecordFromDatabase(string title, string description)
+        {
+            try
+            {
+                using (SQLiteCommand command = new SQLiteCommand("DELETE FROM projects WHERE Title = @Title AND Description = @Description", sqliteConnection))
+                {
+                    sqliteConnection.Open();
+                    command.Parameters.AddWithValue("@Title", title);
+                    command.Parameters.AddWithValue("@Description", description);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error deleting record from database: " + ex.Message);
+            }
+            finally
+            {
+                sqliteConnection.Close();
+            }
+        }
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isEditing)
+            {
+                projectApp.Rows[projectAppView.CurrentCell.RowIndex]["Title"] = titleTextBox.Text;
+                projectApp.Rows[projectAppView.CurrentCell.RowIndex]["Description"] = descriptonTextBox.Text;
+                // Update existing record in the SQLite database
+                UpdateRecordInDatabase();
+            }
+            else
+            {
+                // Add a new row with the correct order of columns
+                projectApp.Rows.Add(null, titleTextBox.Text, descriptonTextBox.Text);
+                // Insert a new record into the SQLite database
+                InsertRecordIntoDatabase();
+            }
+            //Clear fields
+            titleTextBox.Text = "";
+            descriptonTextBox.Text = "";
+            startDatePicker.Value = DateTime.Now; // Reset to default value
+            endDatePicker.Value = DateTime.Now;   // Reset to default value
+            endDatePicker.Checked = false;        // Reset Checked property
+            isEditing = false; //since done with editing
+                               // Show a success message
+                               // Show a pop-up message
+            ShowPopup("Successfully saved!");
+        }
+        //To be able to be in synchrony with my SQLite database I will add a refresh method
+        private void RefreshData()
+        {
+            // Clear existing data in the DataTable
+            projectApp.Rows.Clear();
+
+            // Retrieve data from the SQLite database and populate the DataTable
+            using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM projects", sqliteConnection))
+            {
+                sqliteConnection.Open();
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = Convert.ToInt32(reader["Id"]);
+                        string title = reader["Title"].ToString();
+                        string description = reader["Description"].ToString();
+
+                        // Check if "StartDate" is DBNull or null
+                        DateTime startDate = (reader["StartDate"] == DBNull.Value || reader["StartDate"] == null) ? DateTime.MinValue : Convert.ToDateTime(reader["StartDate"]);
+
+                        // Check if "EndDate" is DBNull or null
+                        DateTime endDate = (reader["EndDate"] == DBNull.Value || reader["EndDate"] == null) ? DateTime.MinValue : Convert.ToDateTime(reader["EndDate"]);
+
+                        // Add the retrieved values to the DataTable
+                        projectApp.Rows.Add(id, title, description, startDate, endDate);
+                    }
+                }
+                sqliteConnection.Close();
+            }
+
+            // Refresh the DataGridView
+            projectApp.AcceptChanges();
+            projectAppView.Refresh();
+        }
+
+        private void InsertRecordIntoDatabase()
+        {
+            using (SQLiteCommand command = new SQLiteCommand("INSERT INTO projects (Title, Description, StartDate, EndDate) VALUES (@Title, @Description, @StartDate, @EndDate)", sqliteConnection))
+            {
+                sqliteConnection.Open();
+                command.Parameters.AddWithValue("@Title", titleTextBox.Text);
+                command.Parameters.AddWithValue("@Description", descriptonTextBox.Text);
+                command.Parameters.AddWithValue("@StartDate", startDatePicker.Value);
+                command.Parameters.AddWithValue("@EndDate", (endDatePicker.Checked) ? (object)endDatePicker.Value : DBNull.Value);
+                command.Parameters.AddWithValue("@Id", projectApp.Rows[projectAppView.CurrentCell.RowIndex]["Id"]);
+                command.ExecuteNonQuery();
+                sqliteConnection.Close();
+            }
+            // Refresh data after making changes
+            RefreshData();
+        }
+
+        private void UpdateRecordInDatabase()
+        {
+            using (SQLiteCommand command = new SQLiteCommand("UPDATE projects SET Title = @Title, Description = @Description, StartDate = @StartDate, EndDate = @EndDate WHERE Id = @Id", sqliteConnection))
+            {
+                sqliteConnection.Open();
+                command.Parameters.AddWithValue("@Title", titleTextBox.Text);
+                command.Parameters.AddWithValue("@Description", descriptonTextBox.Text);
+                command.Parameters.AddWithValue("@StartDate", startDatePicker.Value);
+                command.Parameters.AddWithValue("@EndDate", (endDatePicker.Checked) ? (object)endDatePicker.Value : DBNull.Value);
+
+
+                command.Parameters.AddWithValue("@Id", projectApp.Rows[projectAppView.CurrentCell.RowIndex]["Id"]);
+                command.ExecuteNonQuery();
+                sqliteConnection.Close();
+            }
+            // Refresh data after making changes
+            RefreshData();
+        }
+
+        private async void ShowPopup(string message)
+        {
+            //first method
+            /*  // Use a ToolTip to show a pop-up message for a short duration
+              ToolTip tooltip = new ToolTip();
+              tooltip.Show(message, this, 0, 0, 2000); // Display for 2000 milliseconds (2 seconds)
+
+              // Wait for the duration of the tooltip to disappear
+              await Task.Delay(2000);
+
+              // Dispose the tooltip after it's no longer needed
+              tooltip.Dispose();*/
+
+
+
+            using (var popupForm = new Form())
+            {
+                popupForm.FormBorderStyle = FormBorderStyle.None;
+                popupForm.StartPosition = FormStartPosition.Manual;
+                popupForm.BackColor = Color.Green;
+                popupForm.Size = new Size(200, 100);
+                popupForm.Location = new Point(this.Left + (this.Width - popupForm.Width) / 2, this.Top + (this.Height - popupForm.Height) / 2);
+
+                var label = new Label
+                {
+                    Text = message,
+                    ForeColor = Color.White,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Dock = DockStyle.Fill,
+                    Font = new Font(Font.FontFamily, 12, FontStyle.Bold)
+                };
+
+                popupForm.Controls.Add(label);
+
+                popupForm.Show();
+
+                await Task.Delay(2000); // Display for 2000 milliseconds (2 seconds)
+
+                popupForm.Close();
+            }
+        }
+
+       
+    }
+}
