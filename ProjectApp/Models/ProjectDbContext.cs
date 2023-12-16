94ef7e6057c5768637ba658f0ecd516c0e8a58e6
Fabriceelono <r0912443@student.thomamsore.be>
2023-12-16 01:03:06 +0100

    added the setup exe file

---
+++ ProjectApp/Models/ProjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectApp.Models
+{
+    public class ProjectDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=projects.db");
+        }
+    }
+}
